b9968fdd723b89ec2def7de9eea266231b30ca27
zeusemp <30812825+zeusemp@users.noreply.github.com>
2020-09-13 05:31:44 -0600

    Beer
    Beer

--- server/main.lua
+++ server/main.lua
@@ -1,3 +1,11 @@
 ESX = nil
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
+ESX.RegisterUsableItem('beer', function(source)
+    local _source = source
+    local xPlayer = ESX.GetPlayerFromId(_source)
+
+    xPlayer.removeInventoryItem('beer', 1)
+    TriggerClientEvent('rbd_alcohol:beer')
+end)
+
