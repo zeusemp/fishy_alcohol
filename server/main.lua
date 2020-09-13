ff325d13e92565c3510f1ae1fa4849b2a70b9171
zeusemp <30812825+zeusemp@users.noreply.github.com>
2020-09-13 08:14:31 -0600

    Overdosed System
    Overdosed System

--- server/main.lua
+++ server/main.lua
@@ -3,14 +3,35 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 ESX.RegisterUsableItem('beer', function(source)
     local xPlayer = ESX.GetPlayerFromId(source)
-    TriggerClientEvent('rbd_alcohol:beer', source)
-    xPlayer.removeInventoryItem('beer', 1)
+    local randombeer = math.random(1, 10)
+    if randombeer <= 9 then
+        TriggerClientEvent('rbd_alcohol:beer', source)
+        xPlayer.removeInventoryItem('beer', 1)
+    elseif randombeer = 10 then
+        TriggerClientEvent('rbd_alcohol:overdosed', source)
+    end
 end)
 
 ESX.RegisterUsableItem('tequila', function(source)
     local xPlayer = ESX.GetPlayerFromId(source)
-    TriggerClientEvent('rbd_alcohol:tequila', source)
-    xPlayer.removeInventoryItem('tequila', 1)
+    local random = math.random(1, 10)
+    if random <= 9 then
+        TriggerClientEvent('rbd_alcohol:tequila', source)
+        xPlayer.removeInventoryItem('tequila', 1)
+    elseif random == 10 then
+        TriggerClientEvent('rbd_alcohol:overdosed', source)
+    end
+end)
+
+ESX.RegisterUsableItem('whisky', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)
+    local randomwhisky = math.random(1, 10)
+    if randomwhisky <= 9 then
+        TriggerClientEvent('rbd_alcohol:whisky', source)
+        xPlayer.removeInventoryItem('whisky', 1)
+    else
+        TriggerClientEvent('rbd_alcohol:overdosed', source)
+    end
 end)
 
 RegisterNetEvent('rbd_alcohol:beerConsumption')
@@ -21,4 +42,9 @@ end)
 RegisterNetEvent('rbd_alcohol:tequilaConsumption')
 AddEventHandler('rbd_alcohol:tequilaConsumption', function()
     TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'success', text = 'Acabas de consumir 1x tequila' })
+end)
+
+RegisterNetEvent('rbd_alcohol:whiskyConsumption')
+AddEventHandler('rbd_alcohol:whiskyConsumption', function()
+    TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'success', text = 'Acabas de consumir 1x whisky' })
 end)
