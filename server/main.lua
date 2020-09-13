ed4223ac8792c4b9586588c457a51686e5ec0b57
zeusemp <30812825+zeusemp@users.noreply.github.com>
2020-09-13 08:05:07 -0600

    Added Tequila
    Added Tequila

--- server/main.lua
+++ server/main.lua
@@ -7,7 +7,18 @@ ESX.RegisterUsableItem('beer', function(source)
     xPlayer.removeInventoryItem('beer', 1)
 end)
 
+ESX.RegisterUsableItem('tequila', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)
+    TriggerClientEvent('rbd_alcohol:tequila', source)
+    xPlayer.removeInventoryItem('tequila', 1)
+end)
+
 RegisterNetEvent('rbd_alcohol:beerConsumption')
 AddEventHandler('rbd_alcohol:beerConsumption', function()
     TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'success', text = 'Acabas de consumir 1x cerveza' })
+end)
+
+RegisterNetEvent('rbd_alcohol:tequilaConsumption')
+AddEventHandler('rbd_alcohol:tequilaConsumption', function()
+    TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'success', text = 'Acabas de consumir 1x tequila' })
 end)
