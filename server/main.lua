e36d186c484751cff650dac0b314e807598bdcd3
zeusemp <30812825+zeusemp@users.noreply.github.com>
2020-09-13 06:34:28 -0600

    Beer
    Beer

--- server/main.lua
+++ server/main.lua
@@ -2,10 +2,12 @@ ESX = nil
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 ESX.RegisterUsableItem('beer', function(source)
-    local _source = source
-    local xPlayer = ESX.GetPlayerFromId(_source)
-
+    local xPlayer = ESX.GetPlayerFromId(source)
+    TriggerClientEvent('rbd_alcohol:beer', source)
     xPlayer.removeInventoryItem('beer', 1)
-    TriggerClientEvent('rbd_alcohol:beer')
 end)
 
+RegisterNetEvent('rbd_alcohol:beerConsumption')
+AddEventHandler('rbd_alcohol:beerConsumption', function()
+    TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'success', text = 'Acabas de consumir 1x cerveza' })
+end)
